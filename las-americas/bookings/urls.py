0f3dedc0eba5ce07819c51a5b67f28f48f8b655c
llpw-build <llpw@hotmail.co.uk>
2026-05-13 19:34:32 +0100

    Fixurls formatting error

--- las-americas/bookings/urls.py
+++ las-americas/bookings/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('menu', views.menu, name='menu'),
-    path('book', views.book_table, name='book_table'),
-    path('my-bookings', views.my_bookings, name ='my_bookings'),
-    path('contact', views.contact, name='contact')
+    path('menu/', views.menu, name='menu'),
+    path('book/', views.book_table, name='book_table'),
+    path('my-bookings/', views.my_bookings, name ='my_bookings'),
+    path('contact/', views.contact, name='contact')
 ]
