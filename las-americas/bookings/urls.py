c7a7af4b8d4afb9023be2d35aac3aaad75393d7f
llpw-build <llpw@hotmail.co.uk>
2026-05-13 19:30:18 +0100

    Update urls

--- las-americas/bookings/urls.py
+++ las-americas/bookings/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('menu', views.menu, name='menu'),
+    path('book', views.book_table, name='book_table'),
+    path('my-bookings', views.my_bookings, name ='my_bookings'),
+    path('contact', views.contact, name='contact')
 ]
