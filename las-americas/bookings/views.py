9320539b116968819d88fd1d9e8b7e69374078ff
llpw-build <llpw@hotmail.co.uk>
2026-05-13 19:19:45 +0100

    Add html layout for extra pages and create views

--- las-americas/bookings/views.py
+++ las-americas/bookings/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def book_table(request):
+    return render(request, 'book_table.html')
+
+def my_bookings(request):
+    return render(request, 'my_bookings.html')
+
+def contact(request):
+    return render(request, 'contact.html')
