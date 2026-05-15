db1d0c602266657ef658a622b3054eb00805a32b
llpw-build <llpw@hotmail.co.uk>
2026-05-15 12:14:23 +0100

    Improve admin page

--- las-americas/bookings/admin.py
+++ las-americas/bookings/admin.py
@@ -3,5 +3,26 @@ from .models import RestaurantTable, Booking
 
 # Register your models here.
 
-admin.site.register(RestaurantTable)
-admin.site.register(Booking)
+@admin.register(RestaurantTable)
+class RestaurantTableAdmin(admin.ModelAdmin):
+    list_display = ("table_number", "capacity", "is_available")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "table",
+        "booking_date",
+        "booking_time",
+        "number_of_guests",
+    )
+
+    list_filter = (
+        "booking_date",
+        "table",
+    )
+
+    search_fields = (
+        "user__username",
+    )
